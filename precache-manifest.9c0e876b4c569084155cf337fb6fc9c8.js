3574f32c2195f205fb686aecd482d7d2db88cbdf
Oleg <oleg.irchishyn91@gmail.com>
2020-06-14 16:10:09 +0300

    Updates

---
+++ precache-manifest.9c0e876b4c569084155cf337fb6fc9c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d5bd89747ea2cd8a7df3a7384c6170",
+    "url": "/react-app-skeleton/index.html"
+  },
+  {
+    "revision": "d0d91e456b55ee330c44",
+    "url": "/react-app-skeleton/static/css/main.70872267.chunk.css"
+  },
+  {
+    "revision": "3f6d055b6f6ddf45959a",
+    "url": "/react-app-skeleton/static/js/2.b2302d25.chunk.js"
+  },
+  {
+    "revision": "7e08df03892269e6e39da8a34916b217",
+    "url": "/react-app-skeleton/static/js/2.b2302d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d91e456b55ee330c44",
+    "url": "/react-app-skeleton/static/js/main.1d225744.chunk.js"
+  },
+  {
+    "revision": "9375165dbfee4bb07764",
+    "url": "/react-app-skeleton/static/js/runtime-main.bd167d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-skeleton/static/media/logo.5d5d9eef.svg"
+  }
+]);
